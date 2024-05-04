080f432475d07bfd2abe21fe0ad5f853af8611c4
kata0321 <tpgare3@gmail.com>
2024-05-04 14:57:33 -0700

    tasks

--- models/base_model.py
+++ models/base_model.py
@@ -2,7 +2,9 @@
    """this is a class basemodel"""
 from datetime import datetime
 import uuid
-import json
+import models
+
+
 class BaseModel:
   """Class basemodel  """
   def __init__(self):
@@ -12,12 +14,27 @@ class BaseModel:
     self.updated_at = self.created_at
     
   def __str__(self):
+      """
+
+
+      """
     return "[{}]({}) {}".format(self.__class__.__name__, self.id, self.__dict__)
   
   def save(self):
+      """
+
+
+
+      """
     self.updated_at = datetime.now()
     
   def to_dict(self):
+      """
+
+
+
+
+      """
     obj_dict = self.__dict__.copy()
     obj_dict['__class__'] = self.__class__.__name__
     obj_dict['created_at'] = self.created_at.isoformat()
