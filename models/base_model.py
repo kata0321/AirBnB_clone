96d85d66bd88c035d808b0dfe35b2fcee269819a
kata0321 <tpgare3@gmail.com>
2024-06-18 02:31:29 -0700

    task3

--- models/base_model.py
+++ models/base_model.py
@@ -3,20 +3,15 @@ import uuid
 from datetime import datetime
 
 class BaseModel:
-
     def __init__(self):
-
         self.id = str(uuid.uuid4())
         self.created_at = datetime.now()
         self.updated_at = self.created_at
     def __str__(self):
-
         return f"[{self.__class__.__name__}]({self.id}){self.__dict__}"
     def save(self):
-
         self.updated_at = datetime.now()
     def to_dict(self):
-
         dict_provide = self.__dict__.copy()
         dict_provide['__class__'] = self.__class__.__name__
         dict_provide['created_at'] = self.created_at.isoformat()
