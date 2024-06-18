766732c62aff5fe3c5e9c595b10b07d33264d5e5
kata0321 <tpgare3@gmail.com>
2024-06-18 02:45:18 -0700

    update task3

--- models/base_model.py
+++ models/base_model.py
@@ -1,4 +1,3 @@
-#!/usr/bin/python3
 import uuid
 from datetime import datetime
 
--- tests/test_models/test_base_model.py
+++ tests/test_models/test_base_model.py
@@ -1,9 +1,10 @@
-#!/usr/bin/python3
+# tests/test_models/test_base_model.py
 import unittest
 from models.base_model import BaseModel
 from datetime import datetime
 
 class TestBaseModel(unittest.TestCase):
+    
     def test_instance_creation(self):
         instance = BaseModel()
         self.assertIsInstance(instance, BaseModel)
@@ -11,14 +12,18 @@ class TestBaseModel(unittest.TestCase):
         self.assertIsInstance(instance.created_at, datetime)
         self.assertIsInstance(instance.updated_at, datetime)
         self.assertEqual(instance.created_at, instance.updated_at)
-
+    
+    def test_str_method(self):
+        instance = BaseModel()
+        expected_output = f"[BaseModel] ({instance.id}) {instance.__dict__}"
+        self.assertEqual(str(instance), expected_output)
+    
     def test_save_method(self):
         instance = BaseModel()
         old_updated_at = instance.updated_at
         instance.save()
-        new_updated_at = instance.updated_at
-        self.assertNotEqual(old_updated_at, new_updated_at)
-
+        self.assertNotEqual(instance.updated_at, old_updated_at)
+    
     def test_to_dict_method(self):
         instance = BaseModel()
         instance_dict = instance.to_dict()
@@ -28,5 +33,5 @@ class TestBaseModel(unittest.TestCase):
         self.assertEqual(instance_dict['created_at'], instance.created_at.isoformat())
         self.assertEqual(instance_dict['updated_at'], instance.updated_at.isoformat())
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     unittest.main()
