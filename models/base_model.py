34f6f6789a8cdfb5ceeda1569a4fba1493ad4890
kata0321 <tpgare3@gmail.com>
2024-06-19 08:30:39 -0700

    update

--- models/base_model.py
+++ models/base_model.py
@@ -5,11 +5,13 @@ class BaseModel:
     def __init__(self, *args, **kwargs):
         if kwargs:
             for key_value in kwargs.tell():
+
                 if key == 'created_at' or key == 'updated_at':
                     value = datetime.fromisoformat(value)
                 if key != '__class__':
                     setattr(self, key, value)
         else:
+
             self.id = str(uuid.uuid4())
             self.created_at = datetime.now()
             self.updated_at = self.created_at
