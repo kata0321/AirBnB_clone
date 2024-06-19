7754f5a84c9a93b9e3a1a44b386298082bde6c5a
kata0321 <tpgare3@gmail.com>
2024-06-19 08:13:03 -0700

    update

--- models/base_model.py
+++ models/base_model.py
@@ -7,7 +7,7 @@ class BaseModel:
         self.created_at = datetime.now()
         self.updated_at = self.created_at
     def __str__(self):
-        return f"[{self.__class__.__name__}]({self.id}){self.__dict__}"
+        return f"[{self.__class__.__name__}] ({self.id}) {self.__dict__}"
     def save(self):
         self.updated_at = datetime.now()
     def to_dict(self):
