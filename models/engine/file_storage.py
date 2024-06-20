52006807fb010baead73cfd3d999f7f35ce8b0e4
kata0321 <tpgare3@gmail.com>
2024-06-20 03:03:32 -0700

    tasks

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -11,18 +11,18 @@ class FileStorage:
         """
         Returns the dictionary __objects.
         """
-        return self._objects
+        return self.__objects
     def new(self, obj):
         key = f"{obj.__class__.__name__}.{obj.id}"
         self.__objects[key] = obj
    def save(self):
         with open(self.__file_path, 'w') as f:
             json_objects = {k: v.to_dict() for k, v in self.__objects.items()}
-            json.dump(obj_dict, f)
+            json.dump(json_objects, f)
    def reload(self):
        if os.path.exists(self.__file_path):
             with open(self.__file_path, 'r') as f:
-                obj_dict = json.load(f)
+                json_objects = json.load(f)
                  for obj_dict in json_objects.values():
                     cls_name = obj_dict['__class__']
                     cls = globals()[cls_name]
