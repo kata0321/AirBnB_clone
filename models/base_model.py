64b6bef3e6123a059fc721119a025895caf0fc93
kata0321 <tpgare3@gmail.com>
2024-06-27 02:18:56 -0700

    update

--- models/base_model.py
+++ models/base_model.py
@@ -6,7 +6,6 @@ class BaseModel:
     def __init__(self, *args, **kwargs):
         if kwargs:
             for key_value in kwargs.items():
-
                 if key == 'created_at' or key == 'updated_at':
                       value = datetime.strptime(value, "%Y-%m-%dT%H:%M:%S.%f")
                 setattr(self, key, value)
@@ -15,14 +14,24 @@ class BaseModel:
             self.created_at = datetime.now()
             self.updated_at = self.created_at
             storage.new(self)
+
     def __str__(self):
         return f"[{self.__class__.__name__}] ({self.id}) {self.__dict__}"
+
     def save(self):
         self.updated_at = datetime.now()
         storage.save()
+
     def to_dict(self):
         dict_provide = self.__dict__.copy()
         dict_provide['__class__'] = self.__class__.__name__
         dict_provide['created_at'] = self.created_at.isoformat()
         dict_provide['updated_at'] = self.updated_at.isoformat()
         return dict_provide
+sh: 1: q: not found
+bm1 = BaseModel()
+bm2 = BaseModel(**bm1.to_dict())
+
+print(bm1)
+print(bm2)
+print(bm1.to_dict() == bm2.to_dict())
